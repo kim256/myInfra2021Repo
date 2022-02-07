9c2b7f977b7719ba0e911c4ff0b436d5cf5a5aa6
kim256 <94669203+kim256@users.noreply.github.com>
2022-02-07 12:57:55 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "My2022EC2key" 
+    default     =  "mykey2022" 
 }
 
 variable "instance_type" { 
